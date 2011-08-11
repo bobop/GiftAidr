f4c392be80149cc8d4c49518cbc7e1c4e003b2ea
Ben Gibbs <ben@bobop.co.uk>
2011-08-11 14:11:12 +0100

    added twitter and survey button and validations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,7 @@
 class Donation < ActiveRecord::Base
   belongs_to :user
   attr_protected :user
+  
+  validates_presence_of :charity
+  validates_numericality_of :amount, :message => "is not a number such as 20.00"
 end
